16bb8cec72e3ac2595525f7254cccab965403dda
Lian <13715925424@163.com>
2019-08-30 13:15:50 +0800

    second update

--- src/main.js
+++ src/main.js
@@ -16,10 +16,10 @@ import '@/icons' // icon
 import '@/permission' // permission control
 
 // 如果想线上环境也使用mock数据则使用下面代码
-// import { mockXHR } from '../mock'
-// if (process.env.NODE_ENV === 'production') {
-//   mockXHR()
-// }
+import { mockXHR } from '../mock'
+if (process.env.NODE_ENV === 'production') {
+  mockXHR()
+}
 
 // set ElementUI lang to EN
 Vue.use(ElementUI, { locale })
